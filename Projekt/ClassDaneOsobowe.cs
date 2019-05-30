19c2776daa53dbcbcbf61adb4f4acc4ee98a0039
Rafał Nowrot <rafal.nowrot97@gmail.com>
2019-05-30 23:00:42 +0200

    Init commit

---
+++ Projekt/ClassDaneOsobowe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class ClassDaneOsobowe
+    {
+      
+        List<string> imiona_i_Nazwiska = new List<string>();
+
+        public void DodajImie_i_Nazwisko(string imie_i_Naziwko)
+        {
+            imiona_i_Nazwiska.Add(imie_i_Naziwko);
+        }
+
+        public string Wyswietl_Imie_i_Nazwisko(int index)
+        {
+            return imiona_i_Nazwiska[index];
+            
+        }
+
+    }
+}
---
+++ Projekt/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Program
+    {
+      
+        //public void Wynagrodzenia(double[] args)
+        //{
+        //    double WynZasadnicze;
+        //    double WynZaPraceWNocy;
+        //    double WynZaDyzurPelnionePozaNormalnymiGodzinamiPracy;
+        //    double WynZaCzasNiewykonywaniaPracy;
+        //    double DodatkiZaPraceWGodzinachNadliczonychOrazWNiedzieleISwieta;
+        //}
+
+
+        static void Main(string[] args)
+        {
+            ClassDaneOsobowe DaneOsobowe = new ClassDaneOsobowe();
+            string imie_i_Nazwisko;
+            Console.WriteLine("Ile Pracowników: ");
+            int iloscPracownikow = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dodaj Pracowników ");
+            Console.WriteLine("Podaj Pracownika np Rafal_Nowrot:");
+            for (int i = 0; i < iloscPracownikow; i++)
+            {
+                string j = Console.ReadLine(); 
+                DaneOsobowe.DodajImie_i_Nazwisko(j);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < iloscPracownikow; i++)
+            {
+                Console.WriteLine(DaneOsobowe.Wyswietl_Imie_i_Nazwisko(i));
+            }
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}

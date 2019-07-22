57cc14e48b4723a551d67a554526922fa56ba620
Rafał Nowrot <rafal.nowrot97@gmail.com>
2019-07-22 23:20:01 +0200

    Add ClientConroller, Car Controller

---
+++ DemoSolution.Web/Controllers/CarController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoSolution.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoSolution.Web.Controllers
+{
+    public class CarController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var carService = new CarService();
+            var cars = carService.GetCars();
+
+            return View(cars);
+        }
+    }
+}
---
+++ DemoSolution.Web/Controllers/ClientController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoSolution.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoSolution.Web.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var clientService = new ClientService();
+            var clients = clientService.GetClients();
+
+            return View(clients);
+        }
+    }
+}

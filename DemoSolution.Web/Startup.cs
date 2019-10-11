8a0f6466ac581092a2d5d7d20da4d86f89afa5e6
Rafał Nowrot <rafal.nowrot97@gmail.com>
2019-10-11 14:14:11 +0200

    trying to fix IDENTITY

--- DemoSolution.Web/Startup.cs
+++ DemoSolution.Web/Startup.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using DemoSolution.Web.Models;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
